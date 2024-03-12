6c593df71fd788fbb81ad3d9287994c3e6bbd804
chukotskiyShaman <s.mendrul@g.nsu.ru>
2024-03-12 15:46:05 +0700

    third_task

--- task3/pythonProject1/main.py
+++ task3/pythonProject1/main.py
@@ -3,37 +3,29 @@ from bs4 import BeautifulSoup
 import csv
 
 
-def parse_website(url):
-    headers = {'User-Agent':
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        #код для извлечения нужных данных
-        logo = [soup.find('img')['src'],
-                soup.find('meta', attrs={"name": "keywords"})['content'],
-                soup.find('meta', attrs={"name": "geo.placename"})['content'],
-                soup.find('a', attrs={"class": "logo2023"})['href']]
-        return logo
-    else:
-        print("Ошибка при получении данных", response.status_code)
-        return None
+def parse_cmicki(url, writer):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cmicki = soup.find_all(class_="showcase-item-3")
+    
+    for cmicka in cmicki:
+        # print(cmicka)
+        name = cmicka.find(class_="showcase-name-first").text
+        description = cmicka.find(class_="showcase-name-second").text
+        code = cmicka.find(class_="product-code").text
+        price = cmicka.find(itemprop="price")['content']
+        writer.writerow([name, description, code, price])
 
 
-def save_to_csv(data, filename):
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Some Logo", data[0]])
-        writer.writerow(["Name", data[1]])
-        writer.writerow(["Address", data[2]])
-        writer.writerow(["Logo2023 :D", data[3]])
+if __name__ == "__main__":
+    base_url = 'https://mirm.ru/catalog/orkectrovie-inctrumenti/cmichkovie-inctrumenti/'
+    all_cmicki_data = []
+    with open('cmicki.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Название', 'Описание', 'Артикул', 'Цена'])  
+            for page in range(1,6):
+                url = f'{base_url}?PAGEN_1={page}'
+                parse_cmicki(url, writer)
 
-url = 'http://www.sch112.edusite.ru/'
-parsed_data = parse_website(url)
 
-if parsed_data:
-    save_to_csv(parsed_data, 'parsed_data.csv')
-    print("Данные сохранены в файл")
-else:
-    print("Ошибка при зарписи данных")
+
